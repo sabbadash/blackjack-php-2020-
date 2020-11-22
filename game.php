920ac0d9bb00aac6a57d2cc2d2c4a858533d3fc8
sabbadash <sabbadash@gmail.com>
2020-11-23 00:44:36 +0300

    add basic bootstrap layout

---
+++ game.php
@@ -0,0 +1,141 @@
+<?php
+@session_start();
+
+require 'Player.php';
+
+$player = new Player('player');
+$dealer = new Player('dealer');
+
+if (!isset($_SESSION['dealer_deck'])) {
+    $_SESSION['dealer_deck'] = [];
+    $_SESSION['dealer_points'] = 0;
+    $_SESSION['player_deck'] = [];
+    $_SESSION['player_points'] = 0;
+    $_SESSION['out'] = [];
+    $_SESSION['win'] = false;
+    $_SESSION['status'] = 'in process..';
+    $player->addCard($_SESSION['out']);
+    $player->addCard($_SESSION['out']);
+    $dealer->addCard($_SESSION['out']);
+}
+?>
+<table class="information">
+    <tr>
+<?php
+if (@($_POST['hit'] or $_POST['stand']) and $_SESSION['win'] == true) {
+    session_destroy();
+    $_SESSION['status'] = 'in process..';
+    header("Refresh: 0");
+}
+
+if ($_SESSION['player_points'] == 21 and !$_SESSION['win']) {
+    while ($_SESSION['dealer_points'] < 17) {
+        $dealer->addCard($_SESSION['out']);
+        header("Refresh: 0");
+        if ($_SESSION['dealer_points'] == 21) {
+            $_SESSION['status'] = 'tie game';
+            $_SESSION['win'] = 'tie';
+            continue;
+        } elseif($_SESSION['dealer_points'] >= 17 or $_SESSION['dealer_points'] > 21) {
+            $_SESSION['status'] = 'you won';
+            $_SESSION['win'] = 'player';
+            continue;
+        }
+    }
+}
+
+if ($_SESSION['win'] == 'player') {
+    echo "<td class=\"general-info\" id='win'>Outcome: <br> <em>{$_SESSION['status']}</em></td>";
+} elseif ($_SESSION['win'] == 'dealer') {
+    echo "<td class=\"general-info\" id='lose'>Outcome: <br> <em>{$_SESSION['status']}</em></td>";
+} else {
+    echo "<td class=\"general-info\" id='tie'>Outcome: <br> <em>{$_SESSION['status']}</em></td>";
+}
+
+if (isset($_POST['hit']) and !$_SESSION['win']) {
+    $player->addCard($_SESSION['out']);
+    header("Refresh: 0");
+
+    if ($_SESSION['player_points'] == 21) {
+        while ($_SESSION['dealer_points'] < 17) {
+            $dealer->addCard($_SESSION['out']);
+            header("Refresh: 0");
+            if ($_SESSION['dealer_points'] == 21) {
+                $_SESSION['status'] = 'tie game';
+                $_SESSION['win'] = 'tie';
+                continue;
+            } elseif($_SESSION['dealer_points'] > 21 or $_SESSION['dealer_points'] < 21) {
+                $_SESSION['status'] = 'you won';
+                $_SESSION['win'] = 'player';
+                continue;
+            }
+        }
+    }
+    elseif ($_SESSION['player_points'] > 21) {
+        $_SESSION['status'] = 'dealer won';
+        $_SESSION['win'] = 'dealer';
+    }
+} elseif (isset($_POST['reset'])) {
+    session_destroy();
+    $_SESSION['status'] = 'in process..';
+    header("Refresh: 0");
+} elseif (isset($_POST['stand']) and !$_SESSION['win']) {
+    if ($_SESSION['player_points'] < $_SESSION['dealer_points']) {
+        $_SESSION['status'] = 'dealer won';
+        $_SESSION['win'] = 'dealer';
+    }
+    while ($_SESSION['dealer_points'] < 17) {
+        $dealer->addCard($_SESSION['out']);
+        header("Refresh: 0");
+        if ($_SESSION['player_points'] == 21 and $_SESSION['dealer_points'] == 21) {
+            $_SESSION['status'] = 'tie game';
+            $_SESSION['win'] = 'tie';
+            continue;
+        } elseif ($_SESSION['player_points'] == $_SESSION['dealer_points']) {
+            $_SESSION['status'] = 'tie game';
+            $_SESSION['win'] = 'tie';
+            continue;
+        } elseif ($_SESSION['dealer_points'] > 21 or $_SESSION['player_points'] > $_SESSION['dealer_points']) {
+            $_SESSION['status'] = 'you won';
+            $_SESSION['win'] = 'player';
+            continue;
+        } elseif ($_SESSION['player_points'] < $_SESSION['dealer_points'] or $_SESSION['dealer_points'] == 21) {
+            $_SESSION['status'] = 'dealer won';
+            $_SESSION['win'] = 'dealer';
+            continue;
+        }
+    }
+}
+?>
+</tr>
+</table>
+<table class="cards">
+    <tr>
+        <td><b>Dealer(<?php echo $_SESSION['dealer_points'];?>):</b></td>
+        <?php
+        for ($i = 0; $i < sizeof($_SESSION['dealer_deck']); $i++) {
+            $folder = (sizeof($_SESSION['dealer_deck']) == 1) ? 'closeCards' : 'openCards';
+            echo "<td><img src='images/$folder/{$_SESSION['dealer_deck'][$i]}.gif' height='96' width='71'></td>";
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><b>Your(<?php echo $_SESSION['player_points'];?>):</b></td>
+        <?php
+        for ($i = 0; $i < sizeof($_SESSION['player_deck']); $i++) {
+            echo "<td><img src='images/openCards/{$_SESSION['player_deck'][$i]}.gif' height='96' width='71'></td>";
+        }
+        ?>
+    </tr>
+</table>
+<form class="form" method="POST">
+    <table class="buttons">
+        <tr>
+            <td><input type="submit" name="hit" value="HIT"></td>
+            <td><input type="submit" name="stand" value="STAND"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="reset" value="RESET"></td>
+        </tr>
+    </table>
+</form>
--- index.php
+++ index.php
@@ -1,153 +1,52 @@
-<?php
-@session_start();
-
-require 'Player.php';
-
-$player = new Player('player');
-$dealer = new Player('dealer');
-
-if (!isset($_SESSION['dealer_deck'])) {
-    $_SESSION['dealer_deck'] = [];
-    $_SESSION['dealer_points'] = 0;
-    $_SESSION['player_deck'] = [];
-    $_SESSION['player_points'] = 0;
-    $_SESSION['out'] = [];
-    $_SESSION['win'] = false;
-    $_SESSION['status'] = 'in process..';
-    $player->addCard($_SESSION['out']);
-    $player->addCard($_SESSION['out']);
-    $dealer->addCard($_SESSION['out']);
-}
-
-?>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <title>BlackJack</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>BlackJACK</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-<table class="information">
-    <tr>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <img src="images/icon.png" width="40" height="40" alt="logo">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a href="/" class="nav-link" style="font-size: 20px;">Game</a>
+            </li>
+            <li class="nav-item">
+                <a href="/?about" class="nav-link" style="font-size: 20px;">About</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+<?php if ($_SERVER['REQUEST_URI'] == '/?about'): ?>
+<div class="container-fluid">
+    <p>You can read code there <a href="https://github.com/sabbadash/blackjack-php">here</a></p>
+</div>
 <?php
-if (@($_POST['hit'] or $_POST['stand']) and $_SESSION['win'] == true) {
-    session_destroy();
-    $_SESSION['status'] = 'in process..';
-    header("Refresh: 0");
-}
-
-if ($_SESSION['player_points'] == 21 and !$_SESSION['win']) {
-    while ($_SESSION['dealer_points'] < 17) {
-        $dealer->addCard($_SESSION['out']);
-        header("Refresh: 0");
-        if ($_SESSION['dealer_points'] == 21) {
-            $_SESSION['status'] = 'tie game';
-            $_SESSION['win'] = 'tie';
-            continue;
-        } elseif($_SESSION['dealer_points'] >= 17 or $_SESSION['dealer_points'] > 21) {
-            $_SESSION['status'] = 'you won';
-            $_SESSION['win'] = 'player';
-            continue;
-        }
-    }
-}
-
-if ($_SESSION['win'] == 'player') {
-    echo "<td class=\"general-info\" id='win'>Outcome: <br> <em>{$_SESSION['status']}</em></td>";
-} elseif ($_SESSION['win'] == 'dealer') {
-    echo "<td class=\"general-info\" id='lose'>Outcome: <br> <em>{$_SESSION['status']}</em></td>";
-} else {
-    echo "<td class=\"general-info\" id='tie'>Outcome: <br> <em>{$_SESSION['status']}</em></td>";
-}
-
-if (isset($_POST['hit']) and !$_SESSION['win']) {
-    $player->addCard($_SESSION['out']);
-    header("Refresh: 0");
-
-    if ($_SESSION['player_points'] == 21) {
-        while ($_SESSION['dealer_points'] < 17) {
-            $dealer->addCard($_SESSION['out']);
-            header("Refresh: 0");
-            if ($_SESSION['dealer_points'] == 21) {
-                $_SESSION['status'] = 'tie game';
-                $_SESSION['win'] = 'tie';
-                continue;
-            } elseif($_SESSION['dealer_points'] > 21 or $_SESSION['dealer_points'] < 21) {
-                $_SESSION['status'] = 'you won';
-                $_SESSION['win'] = 'player';
-                continue;
-            }
-        }
-    }
-    elseif ($_SESSION['player_points'] > 21) {
-        $_SESSION['status'] = 'dealer won';
-        $_SESSION['win'] = 'dealer';
-    }
-} elseif (isset($_POST['reset'])) {
-    session_destroy();
-    $_SESSION['status'] = 'in process..';
-    header("Refresh: 0");
-} elseif (isset($_POST['stand']) and !$_SESSION['win']) {
-    if ($_SESSION['player_points'] < $_SESSION['dealer_points']) {
-        $_SESSION['status'] = 'dealer won';
-        $_SESSION['win'] = 'dealer';
-    }
-    while ($_SESSION['dealer_points'] < 17) {
-        $dealer->addCard($_SESSION['out']);
-        header("Refresh: 0");
-        if ($_SESSION['player_points'] == 21 and $_SESSION['dealer_points'] == 21) {
-            $_SESSION['status'] = 'tie game';
-            $_SESSION['win'] = 'tie';
-            continue;
-        } elseif ($_SESSION['player_points'] == $_SESSION['dealer_points']) {
-            $_SESSION['status'] = 'tie game';
-            $_SESSION['win'] = 'tie';
-            continue;
-        } elseif ($_SESSION['dealer_points'] > 21 or $_SESSION['player_points'] > $_SESSION['dealer_points']) {
-            $_SESSION['status'] = 'you won';
-            $_SESSION['win'] = 'player';
-            continue;
-        } elseif ($_SESSION['player_points'] < $_SESSION['dealer_points'] or $_SESSION['dealer_points'] == 21) {
-            $_SESSION['status'] = 'dealer won';
-            $_SESSION['win'] = 'dealer';
-            continue;
-        }
-    }
-}
+    elseif ($_SERVER['REQUEST_URI'] == '/'):
+        include 'game.php';
+    else:
 ?>
-    </tr>
-</table>
-<table class="cards">
-    <tr>
-        <td><b>Dealer(<?php echo $_SESSION['dealer_points'];?>):</b></td>
-        <?php
-        for ($i = 0; $i < sizeof($_SESSION['dealer_deck']); $i++) {
-            $folder = (sizeof($_SESSION['dealer_deck']) == 1) ? 'closeCards' : 'openCards';
-            echo "<td><img src='images/$folder/{$_SESSION['dealer_deck'][$i]}.gif' height='96' width='71'></td>";
-        }
-        ?>
-    </tr>
-    <tr>
-        <td><b>Your(<?php echo $_SESSION['player_points'];?>):</b></td>
-        <?php
-        for ($i = 0; $i < sizeof($_SESSION['player_deck']); $i++) {
-            echo "<td><img src='images/openCards/{$_SESSION['player_deck'][$i]}.gif' height='96' width='71'></td>";
-        }
-        ?>
-    </tr>
-</table>
-<form class="form" method="POST">
-    <table class="buttons">
-        <tr>
-            <td><input type="submit" name="hit" value="HIT"></td>
-            <td><input type="submit" name="stand" value="STAND"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="reset" value="RESET"></td>
-        </tr>
-    </table>
-</form>
+    <h2>Hmmm... Error!</h2>
+<?php
+    endif;
+?>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
 
